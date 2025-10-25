e44a48f17daafe41d1c9aeb632b09968e7f97e02
Dilshad <dilshadalicp28@gmail.com>
2025-10-26 01:57:16 +0530

    req added-11

--- quran_backend/quran_backend/settings.py
+++ quran_backend/quran_backend/settings.py
@@ -158,8 +158,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
+
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ["*"]
+
 
 # Application definition
 INSTALLED_APPS = [
