644cf68fab5b99098c6ed05c1a5bed5d92bb1341
Dilshad <dilshadalicp28@gmail.com>
2025-10-26 02:06:39 +0530

    req added-12

--- quran_backend/quran_backend/urls.py
+++ quran_backend/quran_backend/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('account/', include('adminuser.urls')),
+    path('adminuser/', include('adminuser.urls')),
 
     # Swagger UI at base URL
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
