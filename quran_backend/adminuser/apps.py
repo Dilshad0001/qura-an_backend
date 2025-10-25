21fff7a7597729da2f03beb7218e8519c3f12466
Dilshad <dilshadalicp28@gmail.com>
2025-10-26 01:42:10 +0530

    req added-7

--- quran_backend/adminuser/apps.py
+++ quran_backend/adminuser/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AdminuserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adminuser'
+    name = 'quran_backend.adminuser'
