d543b69ba3705e88a5c573c82336cb92e4ae710f
Dilshad <dilshadalicp28@gmail.com>
2025-11-07 20:26:18 +0530

    error solved

--- quran_backend/user/urls.py
+++ quran_backend/user/urls.py
@@ -3,18 +3,18 @@ from .import views
 
 urlpatterns = [
     # GET all / POST
-    path('surah/', views.SurahAPIView.as_view(), name='surah-list'),
+    path('surah/', views.SurahAPIView.as_view(), name='user-surah-list'),
 
     # GET by ID / PATCH / DELETE
-    path('surah/<int:surah_id>/', views.SurahDetailAPIView.as_view(), name='surah-detail'),
+    path('surah/<int:surah_id>/', views.SurahDetailAPIView.as_view(), name='user-surah-detail'),
 
         # GET all / POST
     path('ayat/', views.AyatAPIView.as_view(), name='ayat-list'),
 
-    # GET by ID / PATCH / DELETE
-    path('ayat/<int:ayat_id>/', views.AyatDetailAPIView.as_view(), name='ayat-detail'),
+    # # GET by ID / PATCH / DELETE
+    # path('ayat/<int:ayat_id>/', views.AyatDetailAPIView.as_view(), name='user-ayat-detail'),
     
-    path('fraction-ayat/', views.FractionAyatAPIView.as_view(), name='fraction-ayat-list'),
-    path('fraction-ayat/<int:fraction_id>/', views.FractionAyatDetailAPIView.as_view(), name='fraction-ayat-detail'),
+    # path('fraction-ayat/', views.FractionAyatAPIView.as_view(), name='user-fraction-ayat-list'),
+    # path('fraction-ayat/<int:fraction_id>/', views.FractionAyatDetailAPIView.as_view(), name='user-fraction-ayat-detail'),
 
 ]
