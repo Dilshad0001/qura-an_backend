2487abaca0e3ae33cc474a503c33efd9e15d3e7a
Dilshad <dilshadalicp28@gmail.com>
2025-11-23 18:00:51 +0530

    issue

--- quran_backend/account/views.py
+++ quran_backend/account/views.py
@@ -103,4 +103,5 @@ class AdminRegisterView(APIView):
                     'email': user.email,
                 }
             }, status=status.HTTP_201_CREATED)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
